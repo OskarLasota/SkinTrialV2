bf81b64de5fd0c376fee2dc5490adec1d8464f41
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-10-11 10:36:23 +0100

    chore: setting up base UI for the project

---
+++ app/src/main/java/com/frezzcoding/skincareadvisor/App.kt
@@ -0,0 +1,14 @@
+package com.frezzcoding.skincareadvisor
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class App : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
---
+++ app/src/main/java/com/frezzcoding/skincareadvisor/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.frezzcoding.skincareadvisor
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.home_view) {
+}
