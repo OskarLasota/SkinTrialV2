596cf088dc5d8700b7f041e51436cd38a3a52d7a
Lasota Oskar <o.lasota@reply.com>
2020-10-29 20:10:27 +0000

    chore: added more fragment views for future functionalities

---
+++ app/src/main/java/com/frezzcoding/skincareadvisor/functionalities/routines/RoutineFragment.kt
@@ -0,0 +1,7 @@
+package com.frezzcoding.skincareadvisor.functionalities.routines
+
+import androidx.fragment.app.Fragment
+import com.frezzcoding.skincareadvisor.R
+
+class RoutineFragment : Fragment(R.layout.routine_view) {
+}
---
+++ app/src/main/java/com/frezzcoding/skincareadvisor/functionalities/scheduler/SchedulerFragment.kt
@@ -0,0 +1,8 @@
+package com.frezzcoding.skincareadvisor.functionalities.scheduler
+
+import androidx.fragment.app.Fragment
+import com.frezzcoding.skincareadvisor.R
+
+class SchedulerFragment : Fragment(R.layout.scheduler_view) {
+
+}
