22daa15bb16125b1d6a98c74800851003a32db87
oskarlasota <frezzyysmite@gmail.com>
2021-07-03 19:35:50 +0100

    chore: refactoring the long methods and putting in Extensions file

--- app/src/main/java/com/frezzcoding/skincareadvisor/utils/Extensions.kt
+++ app/src/main/java/com/frezzcoding/skincareadvisor/utils/Extensions.kt
@@ -1,5 +1,9 @@
 package com.frezzcoding.skincareadvisor.utils
 
+import com.frezzcoding.skincareadvisor.data.Schedule
+import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 fun getDisplayMinutes() : Array<String>{
     return arrayOf("00","01","02","03","04","05","06","07","08",
@@ -13,3 +17,173 @@ fun getDisplayMinutes() : Array<String>{
 fun getDisplayHours() : Array<String>{
     return arrayOf("00","01","02","03","04","05","06","07","08", "09","10","11","12","13","14","15","16","17","18","19","20","21","22","23")
 }
+
+
+fun getNextNotificationDay(schedule : Schedule, day : String) : Long {
+    val calendar = Calendar.getInstance()
+    when(day){
+        "Mon" -> {
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Tue" -> {
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Wed" -> {
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Thu" -> {
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Fri" -> {
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Sat" -> {
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+        "Sun" -> {
+            if (schedule.monday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(1))
+            }
+            if (schedule.tuesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(2))
+            }
+            if (schedule.wednesday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(3))
+            }
+            if (schedule.thursday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(4))
+            }
+            if (schedule.friday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(5))
+            }
+            if (schedule.saturday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(6))
+            }
+            if (schedule.sunday) {
+                return calendar.timeInMillis + (TimeUnit.DAYS.toMillis(7))
+            }
+        }
+    }
+    return 0
+}
+
--- app/src/main/java/com/frezzcoding/skincareadvisor/utils/NotificationBroadcast.kt
+++ app/src/main/java/com/frezzcoding/skincareadvisor/utils/NotificationBroadcast.kt
@@ -19,6 +19,7 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class NotificationBroadcast : BroadcastReceiver() {
@@ -37,14 +38,14 @@ class NotificationBroadcast : BroadcastReceiver() {
         val gson = Gson()
         val json = sharedPreferences.getString(SP_SCHEDULES_KEY, null)
         val type: Type = object : TypeToken<ArrayList<Schedule>>() {}.type
-        var list = gson.fromJson<Any>(json, type) as ArrayList<Schedule>
+        val list : ArrayList<Schedule> = gson.fromJson<Any>(json, type) as ArrayList<Schedule>
 
         for (i in list) {
             if (i.id == key) {
                 schedule = i
             }
         }
-        var builder: NotificationCompat.Builder
+        val builder: NotificationCompat.Builder
         if(this::schedule.isInitialized) {
             builder = NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(R.drawable.logo_small)
@@ -60,232 +61,35 @@ class NotificationBroadcast : BroadcastReceiver() {
                         0
                     )
                 )
-            var notificationManager = NotificationManagerCompat.from(context)
+            val notificationManager = NotificationManagerCompat.from(context)
             createNewNotification(context)
             notificationManager.notify(200, builder.build())
         }
     }
 
     private fun createNewNotification(context: Context) {
-        var time = getNextNotification()
+        val time = getNextNotification()
         if (time == 0L) {
             //don't notify
         } else {
-            var inte = Intent(context, NotificationBroadcast::class.java)
-            inte.putExtra(INTENT_NOTIFICATION_KEY, key)
+            val intent = Intent(context, NotificationBroadcast::class.java)
+            intent.putExtra(INTENT_NOTIFICATION_KEY, key)
             val pi =
-                PendingIntent.getBroadcast(context, key, inte, PendingIntent.FLAG_UPDATE_CURRENT)
+                PendingIntent.getBroadcast(context, key, intent, PendingIntent.FLAG_UPDATE_CURRENT)
             val am = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
             am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pi)
         }
     }
 
+
+
+    //this can cause errors if notification is set for 23:59 but is sent out with a potential delay. This would make the 'today' day different
     private fun getNextNotification(): Long {
+        //get current day
         val calendar = Calendar.getInstance()
-        if (calendar.time.toString().substring(0, 3) == "Mon") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Tue") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Wed") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Thu") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Fri") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Sat") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        if (calendar.time.toString().substring(0, 3) == "Sun") {
-            val calendar1 = Calendar.getInstance()
-            for (i in 0..6) {
-                calendar1.add(Calendar.DATE, 1)
-                when (i) {
-                    0 -> if (schedule.monday) {
-                        return calendar1.timeInMillis
-                    }
-                    1 -> if (schedule.tuesday) {
-                        return calendar1.timeInMillis
-                    }
-                    2 -> if (schedule.wednesday) {
-                        return calendar1.timeInMillis
-                    }
-                    3 -> if (schedule.thursday) {
-                        return calendar1.timeInMillis
-                    }
-                    4 -> if (schedule.friday) {
-                        return calendar1.timeInMillis
-                    }
-                    5 -> if (schedule.saturday) {
-                        return calendar1.timeInMillis
-                    }
-                    6 -> if (schedule.sunday) {
-                        return calendar1.timeInMillis
-                    }
-                }
-            }
-        }
-        return 0
+        val currentDay = calendar.time.toString().substring(0,3)
+
+        return getNextNotificationDay(schedule, currentDay)
     }
 
 
